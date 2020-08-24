8dc95a7239da3df75e98910e807c0b69cb600c65
xyzeng <xzeng@cs.wisc.edu>
2020-08-24 14:38:58 -0500

    local

--- experiments/perc_remote.sh
+++ experiments/perc_remote.sh
@@ -48,8 +48,8 @@ threads=28
 # profile="true"
 # cnt=100000 
 # penalty=50000
-distributed="true"
-local="false"
+distributed="false"
+local="true"
 
 # figure 4: normalized throughput with optimal case, varying requests
 for i in 0 1 2 3 4
