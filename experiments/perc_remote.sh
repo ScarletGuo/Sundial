f8f0ee0b60ab22dccae5f523856ed1efd7cc67d3
xyzeng <xzeng@cs.wisc.edu>
2020-08-25 20:21:31 -0500

    try run 2pc first

--- experiments/perc_remote.sh
+++ experiments/perc_remote.sh
@@ -56,7 +56,7 @@ for i in 0 1 2 3 4
 do
 # for alg in WOUND_WAIT BAMBOO
 # do
-for commit_alg in ONE_PC TWO_PC
+for commit_alg in TWO_PC ONE_PC
 do
 # for perc_remote in 0 0.01 0.02 0.04 0.06 0.08 0.1
 for perc_remote in 0.1
