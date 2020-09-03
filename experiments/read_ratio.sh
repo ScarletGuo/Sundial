5566407a1a1982d30cd7ba3d8ae51bb13820b1b9
xyzeng <xzeng@cs.wisc.edu>
2020-09-02 22:23:36 -0500

    fix table size and new script

---
+++ experiments/read_ratio.sh
@@ -0,0 +1,83 @@
+cd ../
+cp -r config-ycsb-std.h config.h
+
+lognode="false"
+if [ $# -eq 1 ]
+then
+fname="$1"
+elif [ $# -eq 2 ]
+then
+fname="$1"
+lognode="$2"
+else
+fname="log_latency_compute"
+fi
+
+# algorithm
+alg=WAIT_DIE
+commit_alg=ONE_PC
+# latch=LH_MCSLOCK
+# [WW]
+# ww_starv_free="false"
+# [BAMBOO]
+# dynamic="true"
+# retire_on="true"
+# cs_pf="true"
+# opt_raw="true"
+# max_waiter=0
+
+# workload
+wl="YCSB"
+# req=16
+# synthetic=true
+zipf=0
+# num_hs=1
+# pos=SPECIFIED
+# specified=0
+# fixed=1
+# fhs="WR"
+# shs="WR"
+read_ratio=0.5
+# ordered="false"
+# flip=0
+# table_size="10000000"
+# chain="false"
+
+# other
+threads=28
+# profile="true"
+# cnt=100000 
+# penalty=50000
+distributed="false"
+local="false"
+
+# figure 4: normalized throughput with optimal case, varying requests
+# for alg in WOUND_WAIT BAMBOO
+# do
+for commit_alg in TWO_PC ONE_PC
+do
+for i in 0 1 2 3 4 5
+do
+# for perc_remote in 0 0.01 0.02 0.04 0.06 0.08 0.1
+for perc_remote in 0.05
+do
+for log_latency in 0
+do
+for read_ratio in 0 0.2 0.4 0.6 0.8 0.1
+do
+timeout 200 python test.py NETWORK_DELAY=${log_latency} COLLECT_LOCAL_LATENCY=${local} COLLECT_DISTRIBUTED_LATENCY=${distributed} CC_ALG=${alg} COMMIT_ALG=${commit_alg} LOG_NODE=${lognode} NUM_SERVER_THREADS=${threads} PERC_REMOTE=${perc_remote}  READ_PERC=${read_ratio} ZIPF_THETA=${zipf}
+done
+done
+done
+done
+done
+
+cd outputs/
+python3 collect_stats.py
+mv stats.csv ${fname}.csv
+mv stats.json ${fname}.json
+cd ..
+
+if [ "$lognode" = "false" ]; then
+    python3 experiments/send_email.py ${fname}
+fi
