d31e8c03084ca07f51979a9471f112c233dc5b84
ScarletGuo <zguo74@wisc.edu>
2020-10-08 17:13:18 -0500

    fix indentation

--- test_exp.py
+++ test_exp.py
@@ -1,5 +1,4 @@
 import os, sys, re, os.path
-import platform
 import subprocess, datetime, time, signal, json
 from test_distrib import start_nodes, kill_nodes
 from test import compile_and_run, parse_arg
@@ -7,35 +6,35 @@ from test import compile_and_run, parse_arg
 script = "test_distrib.py"
 
 if __name__ == "__main__":
-	job = json.load(open(sys.argv[1]))
-	exp_name = sys.argv[1].split('.')[0]
-	if '/' in exp_name:
-		exp_name = exp_name.split("/")[-1]
+    job = json.load(open(sys.argv[1]))
+    exp_name = sys.argv[1].split('.')[0]
+    if '/' in exp_name:
+        exp_name = exp_name.split("/")[-1]
         curr_node = int(sys.argv[2])
-	args = [""]
-	for key in job:
-		if isinstance(job[key], list):
-			new_args = []
-			for i, x in enumerate(job[key]):
-				for arg in args:
-					arg = arg + "{}={} ".format(key, x)
-					new_args.append(arg)
-			args = new_args
-		else:
-			for arg in args:
-				arg = arg + "{}={} ".format(key, job[key])
-	#if os.path.exists("outputs/stats.json"):
-	#	os.remove("outputs/stats.json")
-	for arg in args:
+    args = [""]
+    for key in job:
+        if isinstance(job[key], list):
+            new_args = []
+            for i, x in enumerate(job[key]):
+                for arg in args:
+                    arg = arg + "{}={} ".format(key, x)
+                    new_args.append(arg)
+            args = new_args
+        else:
+            for arg in args:
+                arg = arg + "{}={} ".format(key, job[key])
+    #if os.path.exists("outputs/stats.json"):
+    #	os.remove("outputs/stats.json")
+    for arg in args:
             if script == "test_distrib.py"
-		ret = start_nodes(script, arg, curr_node)
-		if ret != 0:
-		    continue
-		print("[LOG] KILLING REMOTE SERVER ... ")
-		# kill the remote servers
+        ret = start_nodes(script, arg, curr_node)
+        if ret != 0:
+            continue
+        print("[LOG] KILLING REMOTE SERVER ... ")
+        # kill the remote servers
                 kill_nodes(curr_node)
-		os.system("ssh node-1 'sudo pkill rundb'")
-		print("[LOG] FINISH EXECUTION ")
+        os.system("ssh node-1 'sudo pkill rundb'")
+        print("[LOG] FINISH EXECUTION ")
             else:
                 compile_and_run(parse_arg(arg))
-	print("[LOG] FINISH WHOLE EXPERIMENTS")
+    print("[LOG] FINISH WHOLE EXPERIMENTS")
